97b653d960d3b6b726145169709ab2e926c9feac
jpincar <jpincar@terra.mtv.corp.google.com>
2010-08-30 13:27:18 -0700

    Splitting SDBBackup out.

--- mobile/scripts/checkDaemon.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if ps aux | grep -v grep | grep java | grep Daemon  > /dev/null
-then
-    :
-else
-    echo "AdWhirl Daemon is down" | mail -s "**PROBLEM - AdWhirl Daemon is CRITICAL" youremail@yourdomain.com
-fi
