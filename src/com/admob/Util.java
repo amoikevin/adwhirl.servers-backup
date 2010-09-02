07c0ce4aa12c850cb415729cdc0ab29e1976e705
jpincar <jpincar@terra.mtv.corp.google.com>
2010-09-02 10:47:31 -0700

    Equivilance for SDBBackup

--- src/com/admob/Util.java
+++ src/com/admob/Util.java
@@ -14,7 +14,7 @@ public class Util {
 
     static {
 	DOMAINS = new ArrayList<String>();
-		
+
 	DOMAINS.add("app_customs");
 	DOMAINS.add("apps");
 	DOMAINS.add("networks");
@@ -22,15 +22,20 @@ public class Util {
 	DOMAINS.add("stats");
 	DOMAINS.add("users");
     }
-	
-    public static final String myAccessKey = "CHANGEME";
-    public static final String mySecretKey = "CHANGEME";
+
+    public static final String myAccessKey =
+        "CHANGEME";
+
+
+    public static final String mySecretKey =
+        "CHANGEME";
+
 
     public static AmazonSimpleDB getSDB() {
-	return new AmazonSimpleDBClient(new BasicAWSCredentials(myAccessKey, mySecretKey));	
+	return new AmazonSimpleDBClient(new BasicAWSCredentials(myAccessKey, mySecretKey));
     }
-	
+
     public static AmazonS3 getS3() {
-	return new AmazonS3Client(new BasicAWSCredentials(myAccessKey, mySecretKey));	
+	return new AmazonS3Client(new BasicAWSCredentials(myAccessKey, mySecretKey));
     }
 }
